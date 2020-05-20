69286916a380c72e099e9f14eea9dd6c21e13dbe
Anush Avagyan <57200991+AnushAvagyan@users.noreply.github.com>
2020-05-20 02:46:31 -0700

    Try to configure server to listen not just to local host but to remote machines

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,9 +1,5 @@
-var path = require('path');
 const merge = require('webpack-merge');
 const common = require('./webpack.config.js');
-const SRC_DIR = path.join(__dirname, '/client/src');
-const DIST_DIR = path.join(__dirname, '/client/dist');
-const MomentLocalesPlugin = require('moment-locales-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'development',
