7b597edfec32d2b47f225b33f837cb16ffd83db2
Anush Avagyan <57200991+AnushAvagyan@users.noreply.github.com>
2020-05-27 00:11:21 -0700

    WIP fix styling issues

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -59,7 +59,7 @@ class App extends React.Component {
   render() {
 
     return (
-      <div className="main">
+      <div className="hostServiceSpecific">
         <GlobalStyle />
         { this.state.host &&
 
